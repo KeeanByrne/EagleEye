f6157eb431933004e67419e9c4fe29460faf9d67
Vuyo19 <93484408+Vuyo19@users.noreply.github.com>
2023-09-21 20:48:12 +0200

    Frontend changes

---
+++ app/src/main/java/com/example/eagleeye/Homepage.kt
@@ -0,0 +1,22 @@
+package com.example.eagleeye
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
+import com.example.eagleeye.databinding.ActivityMainBinding
+
+class homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage) // Connecting to the homepage.xml
+
+    }
+}
---
+++ app/src/main/java/com/example/eagleeye/Settings.kt
@@ -0,0 +1,31 @@
+package com.example.eagleeye
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings) // Connecting to the settings.xml
+        populateUnitSpinner(); // Populating the spinner
+
+
+
+    }
+
+    // Populating the unit spinner with Km and Mi.
+    fun populateUnitSpinner() {
+
+        // List of distance units
+        val options = arrayOf("Km", "Mi")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = findViewById<Spinner>(R.id.spinnerMetric)
+        spinner.adapter = adapter
+
+    }
+}

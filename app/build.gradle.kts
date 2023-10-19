3acca95328b333beaaa9f4a3d7410c017523404e
Vuyo19 <93484408+Vuyo19@users.noreply.github.com>
2023-10-19 16:02:55 +0200

    Frontend changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,14 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.cardview:cardview:1.0.0") // Use the latest version
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
-
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.maps:google-maps-services:0.17.0")
 }

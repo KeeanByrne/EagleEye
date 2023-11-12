142198c6da66c791770af6b7392a0e07c701d360
antiixbb <joshuacousins01@gmail.com>
2023-11-12 14:20:31 +0200

    Added Glide support so that stored sightings display with their images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,5 +71,6 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.firebaseui:firebase-ui-firestore:7.2.1")
     implementation("com.squareup.picasso:picasso:2.8")
-
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
--- app/src/main/java/com/example/eagleeye/adapters/SightingFirebaseAdapter.kt
+++ app/src/main/java/com/example/eagleeye/adapters/SightingFirebaseAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.eagleeye.adapters
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.eagleeye.R
 import com.example.eagleeye.models.Sighting
 import com.firebase.ui.firestore.FirestoreArray
@@ -37,11 +40,25 @@ class SightingFirebaseAdapter(
         private val textBirdName: TextView = itemView.findViewById(R.id.birdNameTextView)
         private val textLatinName: TextView = itemView.findViewById(R.id.latinNameTextView)
         private val textLocation: TextView = itemView.findViewById(R.id.locationTextView)
+        private val birdPhotoImageView: ImageView = itemView.findViewById(R.id.birdPhotoImageView)
 
         fun bind(sighting: Sighting) {
-            // Use Picasso or Glide to load the image into the ImageView
-            // Example: Picasso.get().load(sighting.imageURL).into(imageBirdPhoto)
-            Log.d("Sighting Check", "Bind called")
+            // Use Glide to load the image into the ImageView
+            if (sighting.Image != null && sighting.Image.isNotEmpty()) {
+                // Convert Base64 string to byte array
+                val imageByteArray = android.util.Base64.decode(sighting.Image, android.util.Base64.DEFAULT)
+
+                // Load the byte array into the ImageView using Glide
+                Glide.with(itemView)
+                    .load(imageByteArray)
+                    .into(birdPhotoImageView)
+            } else {
+                // If no image is available, you can set a placeholder image
+                Glide.with(itemView)
+                    .load(R.drawable.placeholder_image)
+                    .into(birdPhotoImageView)
+            }
+
             textBirdName.text = "Bird Name: ${sighting.BirdName}"
             textLatinName.text = "Latin Name: ${sighting.LatinName}"
             textLocation.text = "Location: ${sighting.Location}"
